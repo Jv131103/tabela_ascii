f2bb918c85d8e084d7ace7b0364326b891db0c61
Jv131103 <120182670+Jv131103@users.noreply.github.com>
2023-12-29 00:15:50 -0300

    Add files via upload

--- ASCII.py
+++ ASCII.py
@@ -4,13 +4,16 @@ def Ascii(inicial=0, final=None, intervalo=abs(1)):
     # Inserindo o cabeçalho
     cabecalho = ["CHARACTER", "DECIMAL", "OCTAL", "HEXADECIMAL", "BINÁRIO"]
     
-    # Inserindo caracteres especiais, que o python não interpreta de forma que o usuário entenda
+    # Inserindo caracteres especiais, que o Python não interpreta de forma que o usuário entenda
     # Inserindo um "", para que assim na contagem, NULL possa ser iniciado em 0,
     # mas é o primeiro valor de uma lista de 33, ou seja, 32 índices
     caracteres_especiais = ["", "NULL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL",
                             "BS", "HT", "NL", "VT", "NP", "CR", "SO", "SI", "DLE",
                             "DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB", "CAN",
                             "EM", "SUB", "ESC", "FS", "GS", "RS", "US", "SPACE"]
+
+    caracteres_especiais2 = ["", "del", "Ç", "ü", "é", "â", "ä", "à", "å", "ç", "ê", "ë", "è", "ï", "î", "ì",
+                             "Ä", "Å", "É", "æ", "Æ", "ô", "ö", "ò", "û", "ù", "ÿ", "Ö", "Ü", "ø", "£", "Ø", "ƒ", "á", "í"]
 
     dados = []  # Inicializa a lista de dados
 
@@ -19,13 +22,15 @@ def Ascii(inicial=0, final=None, intervalo=abs(1)):
 
     if final:
         for asc in range(inicial, final + 1, intervalo):
-            if inicial > 32:
-                char = chr(asc)
-            elif inicial == 0 and final == 32:
-                char = caracteres_especiais[asc + 1]
+            if 127 <= asc <= 160:
+                try:
+                    char = caracteres_especiais2[(asc - 127) + 1]
+                except:
+                    pass
+            elif asc <= 32:
+                char = caracteres_especiais[asc+1]
             else:
-                char = chr(asc) if 32 < asc else caracteres_especiais[asc + 1]
-
+                char = chr(asc)
             dado = [
                 char,
                 asc,
@@ -35,7 +40,12 @@ def Ascii(inicial=0, final=None, intervalo=abs(1)):
             ]
             dados.append(dado)
     else:
-        char = chr(inicial) if 32 < inicial else caracteres_especiais[inicial + 1]
+        if 127 < inicial < 160:
+            char = caracteres_especiais2[(inicial - 127) + 1]
+        elif inicial < 32:
+            char = caracteres_especiais[inicial+1]
+        else:
+            char = chr(inicial)
         dado = [
             char,
             inicial,
@@ -49,4 +59,4 @@ def Ascii(inicial=0, final=None, intervalo=abs(1)):
 
 if __name__ == "__main__":
     print("BEM VINDO!")
-    ascii(0, 190) #Fique a vontade para verificar os dados, e mudar os valores
+    Ascii(0, 190)  # Fique à vontade para verificar os dados e mudar os valores
